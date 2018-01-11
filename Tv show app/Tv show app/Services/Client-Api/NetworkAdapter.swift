a720b2b4e229ead2f196592629ba8513cac608c8
yves songolo <yves.songolo@gmail.com>
2018-01-11 02:32:53 -0800

    set up tv show manager

--- Tv show app/Tv show app/Services/Client-Api/NetworkAdapter.swift
+++ Tv show app/Tv show app/Services/Client-Api/NetworkAdapter.swift
@@ -29,3 +29,5 @@ struct NetworkAdapter{
         }
     }
 }
+
+
---
+++ Tv show app/Tv show app/Services/Client-Api/TVShowManager.swift
@@ -0,0 +1,28 @@
+//
+//  TVShowManager.swift
+//  Tv show app
+//
+//  Created by Yveslym on 1/11/18.
+//  Copyright © 2018 Yveslym. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+struct TVSHowManager{
+    func findSerie(title:  String){
+        
+    }
+    func findEpisode(episode Id: Int){
+        
+    }
+    func popularTvShow(){
+        
+    }
+    func bestRateTvShow(){
+        
+    }
+    func similarTvShow(tvShow Id: Int){
+        
+    }
+}
